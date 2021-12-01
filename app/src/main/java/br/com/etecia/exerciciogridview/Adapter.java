622805292168b2671873febd077b934ec147320f
GabrielAlbSilv <gabrielalbuquerquemoura@gmai.com>
2021-12-01 17:04:01 -0300

    nova commit x2

--- app/src/main/java/br/com/etecia/exerciciogridview/Adapter.java
+++ app/src/main/java/br/com/etecia/exerciciogridview/Adapter.java
@@ -1,11 +1,15 @@
 package br.com.etecia.exerciciogridview;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.List;
@@ -30,24 +34,54 @@ public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
 
         return new ViewHolder(view);
     }
-    //Será realizada a montagem dos dados.
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, final int position) {
 
         //passando valores para o RecyclerView
         holder.tvNome.setText(dado.get(position).getNome());
+        holder.tvIdade.setText(dado.get(position).getIdade());
+        holder.tvPontuacao.setText(dado.get(position).getPontuacao());
         holder.imgPers.setImageResource(dado.get(position).getMiniatura());
+        holder.cardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(mContext, Personagem.class);
+
+                intent.putExtra("Nome", dado.get(position).getNome());
+                intent.putExtra("Idade", dado.get(position).getIdade());
+                intent.putExtra("Pontuacao", dado.get(position).getPontuacao());
+                intent.putExtra("Miniatura", dado.get(position).getMiniatura());
+
+                mContext.startActivity(intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK));
+            }
+        });
     }
+
 
     @Override
     public int getItemCount() {
-        return 0;
+       return dado.size();
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder{
 
+        TextView tvNome, tvIdade, tvPontuacao;
+        ImageView imgPers;
+        CardView cardView;
+
+
         public ViewHolder(@NonNull View itemView) {
+
             super(itemView);
+
+            tvNome = itemView.findViewById(R.id.idNome);
+            tvIdade = itemView.findViewById(R.id.idIdad);
+            tvPontuacao = itemView.findViewById(R.id.idPont);
+            imgPers = itemView.findViewById(R.id.ImgPerso);
+            cardView = itemView.findViewById(R.id.idCardView);
+
+
         }
     }
 }
